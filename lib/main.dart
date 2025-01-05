23c2647c1e865a3ba22e2f95dbe639f6265cffb6
Manohar Acharya <manoharacharya63@gmail.com>
2025-01-05 12:01:05 +0530

    5. showing chat screen if user is already loged-in

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:chat_app/firebase_options.dart';
 import 'package:chat_app/screens/auth.dart';
+import 'package:chat_app/screens/chat.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
@@ -17,7 +19,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: AuthScreen(),
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return const ChatScreen();
+          }
+          return const AuthScreen();
+        },
+      ),
     );
   }
 }
